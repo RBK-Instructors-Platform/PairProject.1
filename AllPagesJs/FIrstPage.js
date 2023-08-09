049c59c5b71a21a6d4a559165afb3425685aead1
Mahran07 <mahranbhr10@gmail.com>
2023-08-09 22:52:11 +0200

    Page2AddingPicsAndSomeAnimation

--- AllPagesJs/FIrstPage.js
+++ AllPagesJs/FIrstPage.js
@@ -1,5 +1,5 @@
 $("#btn0").on('click',function(){
-    window.location.href='http://127.0.0.1:5500/PairProject.1/AllPagesHtml/2Page.html'
+    window.location.href='/AllPagesHtml/2Page.html'
 })
 
 $("#btn0").on({
